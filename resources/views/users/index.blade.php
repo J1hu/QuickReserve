f748fb68119401bb04356dd5c5567ae931e5cf97
jcasimiro <j.casimiro@qstrike.com>
2024-11-04 11:37:30 +0800

    Add styling to list of users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,16 +9,31 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="text-lg">USER PAGE</div>
+<body class="p-6 bg-gray-100">
+    <div class="mb-4 text-lg font-semibold">USER PAGE</div>
+
+    <div class="overflow-x-auto rounded-lg shadow-md">
+        <table class="w-full overflow-hidden bg-white border border-gray-300 rounded-lg">
+            <thead>
+                <tr class="bg-gray-100 border-b">
+                    <th class="px-4 py-2 font-medium text-left text-gray-600">Name</th>
+                    <th class="px-4 py-2 font-medium text-left text-gray-600">Email</th>
+                    <th class="px-4 py-2 font-medium text-left text-gray-600">Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr class="border-b last:border-none">
+                        <td class="px-4 py-3 font-semibold text-gray-800">{{ $user->name }}</td>
+                        <td class="px-4 py-3 text-gray-800">{{ $user->email }}</td>
+                        <td class="px-4 py-3 text-gray-800">{{ $user->role }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
 </body>
-<div class="table">
-    @foreach ($users as $user)
-        <div>Name: {{ $user->name }}</div>
-        <div>Email: {{ $user->email }}</div>
-        <div>Role: {{ $user->role }}</div>
-        <hr>
-    @endforeach
-</div>
+
 
 </html>
