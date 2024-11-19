f1b21a7c78ba2b3f368ac161da689ad09244242d
jcasimiro <j.casimiro@qstrike.com>
2024-11-19 19:26:33 +0800

    Optimize web routes

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// AUTHENTICATION
+// Authentication routes
+Route::group(['controller' => AuthController::class], function () {
+    Route::get('/register', 'showRegisterForm')->name('showRegisterForm');
+    Route::post('/register', 'register')->name('register');
+    Route::get('/login', 'showLoginForm')->name('showLoginForm');
+    Route::post('/login', 'login')->name('login');
 
-// REGISTER
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('showRegisterForm');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-// LOGIN
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('showLoginForm');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-// LOGOUT
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware(AuthMiddleware::class);
-// DASHBOARD
-Route::get('/dashboard', [AuthController::class, 'showDashboardPage'])->name('showDashboardPage')->middleware(AuthMiddleware::class);
+    // Protected auth routes
+    Route::middleware(AuthMiddleware::class)->group(function () {
+        Route::post('/logout', 'logout')->name('logout');
+        Route::get('/dashboard', 'showDashboardPage')->name('showDashboardPage');
+    });
+});
 
-//RESOURCE
-// USERS
-Route::get('/users', [UserController::class, 'showAllUsers'])->name('showAllUsers')->middleware(AuthMiddleware::class);
-Route::get('/users/create', [UserController::class, 'createUser'])->name('createUser')->middleware(AuthMiddleware::class);
-Route::post('/users/store', [UserController::class, 'storeUser'])->name('storeUser')->middleware(AuthMiddleware::class);
+// User resource routes
+Route::group([
+    'prefix' => 'users',
+    'controller' => UserController::class,
+    'middleware' => AuthMiddleware::class
+], function () {
+    Route::get('/', 'showAllUsers')->name('showAllUsers');
+    Route::get('/create', 'createUser')->name('createUser');
+    Route::post('/store', 'storeUser')->name('storeUser');
+});
